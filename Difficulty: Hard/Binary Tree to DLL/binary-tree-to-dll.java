47d53efa525a1927855a511118c57193cc33dd8d
Sujeet Yadav <yadavsuzeet763@gmail.com>
2024-09-15 22:46:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Binary Tree to DLL/binary-tree-to-dll.java
@@ -0,0 +1,181 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}
+class BT_To_DLL
+{
+    
+    static Node buildTree(String str)throws IOException{
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+
+	void inorder(Node node)
+	{
+		if(node==null)
+			return ;
+		else
+			inorder(node.left);
+			System.out.print(node.data + " ");
+			inorder(node.right);
+	}
+	public static void printList(Node head) 
+    {
+		Node prev = head;
+        while (head != null) 
+        {
+            System.out.print(head.data + " ");
+			prev = head;
+            head = head.right;
+        }
+		
+		System.out.println();
+		while(prev != null)
+		{
+			System.out.print(prev.data+" ");
+			prev = prev.left;
+		}
+    }
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			    
+			    Node ans = g.bToDLL(root);
+			    printList(ans);
+                t--;
+                System.out.println();
+	        }
+			
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}*/
+
+//This function should return head to the DLL
+
+class Solution {
+    // Function to convert binary tree to doubly linked list and return it.
+    Node bToDLL(Node root) {
+        // Collecting the in-order traversal of the binary tree
+        ArrayList<Integer> list = inorder(root);
+        Node cur = new Node(list.get(0)); // Create the head of the DLL
+        Node prev = cur; // Keep track of the previous node
+
+        // Iterate through the collected values to create the DLL
+        for (int i = 1; i < list.size(); i++) {
+            Node newNode = new Node(list.get(i)); // Create a new node for the DLL
+            prev.right = newNode; // Link the previous node's right to the new node
+            newNode.left = prev; // Link the new node's left to the previous node
+            prev = newNode; // Move the previous pointer to the new node
+        }
+
+        return cur; // Return the head of the doubly linked list
+    }
+
+    // Helper function to perform in-order traversal
+    ArrayList<Integer> inorder(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null) return list; // Base case for recursion
+
+        // Traverse the left subtree, visit the root, and then the right subtree
+        list.addAll(inorder(root.left));
+        list.add(root.data);
+        list.addAll(inorder(root.right));
+
+        return list; // Return the collected values
+    }
+}
