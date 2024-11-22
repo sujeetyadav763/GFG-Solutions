7297495fa000310a9d3e7069b1342e8de3a8e638
Sujeet Yadav <yadavsuzeet763@gmail.com>
2024-11-22 21:24:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int prices[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                prices[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int res = obj.maximumProfit(prices);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maximumProfit(int prices[]) {
+      int ans = 0;
+      int min_Price = Integer.MAX_VALUE;
+      for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min_Price) {
+                min_Price = prices[i];
+            }
+            int profit = prices[i] - min_Price;
+            if (profit > ans) {
+                ans = profit;
+            }
+        }
+        return ans;
+    }
+}
